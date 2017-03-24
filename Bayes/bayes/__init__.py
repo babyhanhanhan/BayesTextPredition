b47fbdb348139a95f4649f708b54c159d9a9a277
Pan Haiqi <978005581@qq.com>
2017-03-24 18:49:28 +0800

    update code

--- Bayes/bayes/__init__.py
+++ Bayes/bayes/__init__.py
@@ -6,7 +6,7 @@ import jieba
 from numpy import *
 import numpy as np
 
-f = codecs.open("D:\\emotion-analysis\\data\\pnn_annotated.txt",'r',encoding='utf-8')
+f = codecs.open("D:\\emotion-analysis\\data\\f_4.txt",'r',encoding='utf-8')
 data = f.readlines()
 newdata = []
 segmenttest = []
@@ -75,26 +75,77 @@ for line in splitnegative:
 for line in splitneutral:
     listclass.append(0)
 print(splitdata)
-print(listclass)
-f1 = codecs.open("D:\\textpredition\\emotionanalyse\\positive.txt",'r',encoding='utf-8')
-data1 = f1.readlines()
-newdata1 = data1[1:]
-
-f2 = codecs.open("D:\\textpredition\\emotionanalyse\\negative.txt",'r',encoding='utf-8')
-data2 = f2.readlines()
-
-newdata2 = data2[1:]
-
-segment1 = []
-segment2 = []
-segment = []
-
+######################################################################################
+f = codecs.open("D:\\emotion-analysis\\data\\f_1.txt",'r',encoding='utf-8')
+data1 = f.readlines()
+newdata1 = []
+segmenttest1 = []
+newsegmenttest1 = []
+for line in data1:
+    newdata1.append(line.strip('\r\n'))
 for line in newdata1:
-    segment1.append(line.strip(' \r\n'))
-for line in newdata2:
-    segment2.append(line.strip(' \r\n'))
+    segmenttest1.append(line.replace('\t',''))
+    
+classpositive1 = []
+classnegative1 = []
+classneutral1 = []
+
+temp11 = []
+temp21 = []
+temp31 = []
+
+for line in segmenttest1:
+    if line[0] == '1':
+        classpositive1.append(line[1:])
+    if line[0] == '0':
+        classneutral1.append(line[1:])
+    if line[0] == '-':
+        classnegative1.append(line[2:])
+        
+splitpositive1 = []
+splitnegative1 = []
+splitneutral1 = []
 
 
+for line in classpositive1:
+    temp11.append(jieba.cut(line))
+for line in classneutral1:
+    temp21.append(jieba.cut(line))
+for line in classnegative1:
+    temp31.append(jieba.cut(line))
+    
+i1 = 0
+i2 = 0
+i3 = 0
+for line in temp11:
+    splitpositive1.append([])
+    for seg in line:
+        if seg != '\r\n':
+            splitpositive1[i1].append(seg)
+    i1 += 1      
+for line in temp21:
+    splitneutral1.append([])
+    for seg in line:
+        if seg != '\r\n':
+            splitneutral1[i2].append(seg)
+    i2 += 1
+for line in temp31:
+    splitnegative1.append([])
+    for seg in line:
+        if seg != '\r\n':
+            splitnegative1[i3].append(seg)
+    i3 += 1
+
+splitdata1 = splitpositive1 + splitnegative1 +splitneutral1
+listclass1 = []
+for line in splitpositive1:
+    listclass1.append(1)
+for line in splitnegative1:
+    listclass1.append(2)
+for line in splitneutral1:
+    listclass1.append(0)
+print(listclass1)
+##############################################################################
 class NBayes(object):
     def __init__(self):
         self.vocabulary = []
@@ -143,7 +194,8 @@ class NBayes(object):
     def map2vocab(self,testdata):
         self.testset = np.zeros([1,self.vocablen])
         for word in testdata:
-            self.testset[0,self.vocabulary.index(word)] += 1
+            if word in self.vocabulary:
+                self.testset[0,self.vocabulary.index(word)] += 1
         
     def predict(self,testset):
         if np.shape(testset)[1] != self.vocablen:
@@ -160,9 +212,13 @@ class NBayes(object):
     
 count = 0
 nb = NBayes() 
+newlistclass = []
 nb.train_set(splitdata,listclass)
-for i in range(len(splitdata)):
-    nb.map2vocab(splitdata[i])
-    if nb.predict(nb.testset) == listclass[i]:
+for i in range(len(splitdata1)):
+    nb.map2vocab(splitdata1[i])
+    newlistclass.append(nb.predict(nb.testset))
+    if nb.predict(nb.testset) == listclass1[i]:
         count += 1
-print ('rate:',count/len(splitdata))
+#print (listclass)
+print (newlistclass)
+print ('rate:',count/len(splitdata1))
